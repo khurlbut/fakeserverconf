8d41b492bd14f965391e30338f7f32fa4b6b11b2
Kevin Hurlbut <kevinhurlbut@sbcglobal.net>
2018-11-12 15:48:17 -0800

    initial functioning configurator.

--- configurator.go
+++ configurator.go
@@ -1,18 +1,47 @@
 package fakeserverconf
 
-type Configuration struct {
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
 	Path   string
 	Status int
 	Body   string
 }
 
-func Configurator() []*Configuration {
-	c := &Configuration{
+type Configuration []*Config
+
+func DefaultConfig() Configuration {
+	c := &Config{
 		Path:   "/",
 		Status: 200,
 		Body:   "Welcome to fakeserver. This is the Default configuration for Root path (/)",
 	}
-	configs := []*Configuration{c}
+	configs := []*Config{c}
 
 	return configs
+}
+
+func ReadJSON(json string) Configuration {
+	return unmarshal([]byte(json))
+}
+
+func ReadJSONFile(path string) Configuration {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return unmarshal(bytes)
+}
+
+func unmarshal(bytes []byte) Configuration {
+	var c Configuration
+	err := json.Unmarshal(bytes, &c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c
 }
--- configurator_test.go
+++ configurator_test.go
@@ -7,12 +7,29 @@ import (
 )
 
 var _ = Describe("Configurator Tests", func() {
-	c := Configurator()
 
 	It("should return a default Root page config", func() {
+		c := DefaultConfig()
 		Ω(c[0].Path).Should(Equal("/"))
 		Ω(c[0].Status).Should(Equal(200))
 		Ω(c[0].Body).Should(Equal("Welcome to fakeserver. This is the Default configuration for Root path (/)"))
+	})
+
+	It("should read a single config from a JSON string", func() {
+		c := ReadJSON(`[{"Path":"/","Status": 200,"Body":"This is from a JSON string"}]`)
+		Ω(c[0].Path).Should(Equal("/"))
+		Ω(c[0].Status).Should(Equal(200))
+		Ω(c[0].Body).Should(Equal("This is from a JSON string"))
+	})
+
+	It("should read multiple configs from a JSON string array", func() {
+		c := ReadJSON(`[{"Path":"/","Status": 200,"Body":"Body 1"},{"Path":"/","Status": 200,"Body":"Body 2"}]`)
+		Ω(c[0].Path).Should(Equal("/"))
+		Ω(c[0].Status).Should(Equal(200))
+		Ω(c[0].Body).Should(Equal("Body 1"))
+		Ω(c[1].Path).Should(Equal("/"))
+		Ω(c[1].Status).Should(Equal(200))
+		Ω(c[1].Body).Should(Equal("Body 2"))
 	})
 
 })
